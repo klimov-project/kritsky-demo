63de88e0816764ef6d72fc0060b31ee4b8aa6ba5
John Doe <ivgotthepower@proton.me>
2026-05-05 20:33:52 +0300

    build: fix assets path, add env NEXT_PUBLIC_BASE_PATH

--- adminka/next.config.ts
+++ adminka/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  basePath: '/adminka',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '/adminka',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '/adminka',
   trailingSlash: true,
   // Игнорировать автоматические редиректы
   skipTrailingSlashRedirect: true,
