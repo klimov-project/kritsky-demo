925f2790306a03d59c21a64f67a72dc9b1949952
John Doe <ivgotthepower@proton.me>
2026-05-06 13:43:52 +0300

    build: fix env from docker-compose

--- adminka/next.config.ts
+++ adminka/next.config.ts
@@ -3,19 +3,9 @@ import type { NextConfig } from 'next';
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '/adminka';
 
 const nextConfig: NextConfig = {
-  basePath: basePath,
-  assetPrefix: basePath,
+  basePath: '/adminka',
+  // assetPrefix: '/adminka',
   trailingSlash: true,
-  skipTrailingSlashRedirect: true,
-
-  // Важно для Next.js 16+
-  output: 'standalone', // если используете Docker
-
-  // Явно указать пути к статике
-  distDir: '.next',
-
-  // Для правильной работы с прокси
-  allowedDevOrigins: ['62.113.99.250', 'localhost'],
 };
 
 export default nextConfig;
